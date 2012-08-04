effddbe1ad3b73be3fbb798755278f9681c12f9d
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2012-08-04 17:57:04 -0400

    starting the emulator before tests run

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,20 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '../../', 'lib'))
 
 require 'rspec/expectations'
+require 'childprocess'
 require 'ADB'
 
 World(ADB)
+
+emulator = ChildProcess.build('emulator', '-avd', 'Android_4.0.3')
+emulator.start
+
+at_exit do
+  emulator.stop
+end
+
+
+After do
+  stop_server
+end
+
--- features/support/hooks.rb
+++
@@ -1,4 +0,0 @@
-
-After do
-  stop_server
-end
