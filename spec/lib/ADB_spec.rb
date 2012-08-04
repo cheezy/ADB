a4f5920ed79c13d9291af147f3d5d869090b2651
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2012-08-04 18:00:08 -0400

    added spec for killing server

--- spec/lib/ADB_spec.rb
+++ spec/lib/ADB_spec.rb
@@ -5,6 +5,11 @@ describe ADB do
     ChildProcess.should_receive(:build).with('adb', 'start-server').and_return(process_mock)
     ADB.start_server
   end
+
+  it "should know how to stop the adb server" do
+    ChildProcess.should_receive(:build).with('adb', 'kill-server').and_return(process_mock)
+    ADB.stop_server
+  end
   
   it "should know how to provide list of devices" do
     ChildProcess.should_receive(:build).with('adb', 'devices').and_return(process_mock)
