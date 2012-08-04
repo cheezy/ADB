8ed96acc8cfc62f5b4e97090025f152e72110621
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2012-08-04 10:35:08 -0400

    shell for specs

---
+++ spec/lib/ADB_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe ADB do
+  it "should know how to provide list of devices" do
+    
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+# encoding: utf-8
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
+require 'spec'
+require 'ADB'
+
