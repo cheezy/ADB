8dfaacbee967d4ce5c330a50fe03d022d671a40f
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2012-08-05 08:12:10 -0400

    added spec for stopping process after timeout

--- spec/lib/ADB_spec.rb
+++ spec/lib/ADB_spec.rb
@@ -15,4 +15,16 @@ describe ADB do
     ChildProcess.should_receive(:build).with('adb', 'devices').and_return(process_mock)
     ADB.devices
   end
+
+  it "should stop process if it takes too long" do
+    process = double('process_mock')
+    process.should_receive(:io).twice.and_return(process)
+    process.should_receive(:stdout=)
+    process.should_receive(:stderr=)
+    process.should_receive(:start)
+    process.should_receive(:poll_for_exit).and_raise(ChildProcess::TimeoutError)
+    process.should_receive(:stop)
+    ChildProcess.should_receive(:build).with('adb', 'devices').and_return(process)
+    ADB.devices
+  end
 end
