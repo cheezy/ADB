6a04c694b96101684e83aae80ad8322cf429218f
Levi Wilson <levi@leviwilson.com>
2012-09-08 23:07:49 -0400

    add rdoc info for instrument method

--- lib/ADB/instrumentation.rb
+++ lib/ADB/instrumentation.rb
@@ -2,6 +2,21 @@ require 'ADB/errors'
 
 module ADB
   module Instrumentation
+    #
+    # send instrumentation requests
+    # 
+    # @example
+    #   instrument "com.example/android.test.InstrumentationTestRunner"
+    #   # will run all of the tests within the 'com.example' package using the 'android.test.InstrumentationTestRunner'
+    #
+    # @example
+    #   instrument "com.example/android.test.InstrumentationTestRunner", :class => "com.example.test.SomeTestClass"
+    #
+    #   # will execute all of the tests within 'com.example.test.SomeTestClass'
+    #
+    # @param [String] runner indicates the package/runner to instrument
+    # @param [Hash] collection of key/value pairs to be sent as arguments to the instrumentation runner
+    #
     def instrument(runner, args = {})
       with(the(runner) << and_the(args))
     end
